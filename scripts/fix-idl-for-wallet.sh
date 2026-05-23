4676ee9cabbaffdec2d35ae9d920d0e2fe1a4839
OneTrueHomie <vicigwilo@gmail.com>
2026-05-23 13:02:54 +0100

    fix(scripts): update IDL interface hash replacements for vara-wallet

--- scripts/fix-idl-for-wallet.sh
+++ scripts/fix-idl-for-wallet.sh
@@ -7,7 +7,7 @@ if [ ! -f "$IDL" ]; then
   exit 1
 fi
 sed -i '' \
-  -e 's/0xc3aae4627b50f054/0x6e6c0e38efa38a43/g' \
+  -e 's/0x9aa74b14277a5770/0x7d0d4dfb0f73a29c/g' \
   -e 's/0x1017ab3b5a2c733c/0x7a087345765a3790/g' \
   -e 's/0x5e3679ba33d6775f/0x47a8dd7c5aa52256/g' \
   "$IDL"
